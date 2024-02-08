93c9ef0b6ff983c4bec0da1110a2d25a153ca290
mark <140693604+Markysf@users.noreply.github.com>
2024-02-08 15:00:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ btnNo.addEventListener("mouseover", (event) => {
 
   let newTop = btnTop;
   let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 6) {
+  while (Math.abs(newTop - btnTop) < containerHeight / 10) {
     newTop = getRandomNumber(0, containerHeight - btnHeight);
   }
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 6) {
+  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
     newLeft = getRandomNumber(0, containerWidth - btnWidth);
   }
 
